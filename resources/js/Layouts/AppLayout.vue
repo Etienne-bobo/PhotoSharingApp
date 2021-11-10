c20ccae9835268af607adc33380f2083f72dfa33
etienne.blavo <etiennebobo17@gmail.com>
2021-11-10 17:57:31 +0530

    feat: app layout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen">
     <nav class="bg-white border-gray-100">
       <!-- Primary Navigation Menu -->
-      <div class="max-w-7xl mx-auto py-1 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-7xl mx-auto py-1 px-4 lg:px-0">
         <div class="flex justify-between h-16">
           <div class="flex">
             <!-- Logo -->
